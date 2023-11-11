3435891a83b9ed0d5fd319ff7749b0068693b54f
lehanineFaical <120819770+lehanineFaical@users.noreply.github.com>
2023-11-11 23:32:45 +0100

    Delete dataToObject.h

--- dataToObject.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATATOOBJECT_H
-#define DATATOOBJECT_H
-#include "dataToObject.c"
-struct Tclient creerClient(char nom[],char prenom[],char adresse[],int id);//done
-struct Treservation creerResevation(int id,struct Tdate arvDate,struct Tdate depDate,int roomNbr,int clientId);
-struct Tdate creerDate(int DD,int MM,int YY);//done
-#endif
